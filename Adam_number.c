1ab131d8297ae62a024853c2f969f896a6aae278
rampavan21 <121847895+rampavan21@users.noreply.github.com>
2023-05-25 19:53:50 +0530

    #include<stdio.h> int main() {     int n,s=0,a,i,b,pro;     scanf("%d",&n);     a=n*n;     for(i=n;i>0;i=i/10)     {         b=i%10;         s=s*10+b;     }     pro=s*s;     s=0;     for(i=pro;i>0;i=i/10)     {         b=i%10;         s=s*10+b;     }     if(s==a)     printf("True");     else     printf("False"); }

--- Adam_number.c
+++ Adam_number.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
-int main(){
-    int a,sq1,sq2,r1,r2,ans1=0,ans2=0,i,j;
-    scanf("%d",&a);
-    int b=a;
-    sq1=a*a;
-    for(i=a;a!=0;a=a/10)
+int main()
+{
+    int n,s=0,a,i,b,pro;
+    scanf("%d",&n);
+    a=n*n;
+    for(i=n;i>0;i=i/10)
     {
-        r1=a%10;
-        ans1=ans1*10 + r1;
+        b=i%10;
+        s=s*10+b;
     }
-    sq2=ans1*ans1;
-    for(i=sq2;sq2!=0;sq2=sq2/10){
-        r2=sq2%10;
-        ans2=ans2*10+r2;
+    pro=s*s;
+    s=0;
+    for(i=pro;i>0;i=i/10)
+    {
+        b=i%10;
+        s=s*10+b;
     }
-    if(ans2==sq1) printf("True");
+    if(s==a)
+    printf("True");
     else
-    {
-        printf("False");
-    }
+    printf("False");
 }
